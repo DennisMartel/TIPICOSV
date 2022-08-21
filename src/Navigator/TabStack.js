ccebb3eb3db718a63fbaa623b879e11f2e7fa05a
Dennis Martel <marteldennis05@gmail.com>
2022-08-21 14:28:30 -0600

    organize code tab navigator

--- src/Navigator/TabStack.js
+++ src/Navigator/TabStack.js
@@ -1,6 +1,4 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet } from "react-native";
-import { AntDesign } from 'expo-vector-icons';
 
 import Home from "../screens/Home";
 import Categories from "../screens/Categories";
@@ -12,33 +10,8 @@ import BottomTabBar from "../components/BottomTabBar";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-    
-    const screensOptions = (route, color) => {
-        let iconName;
-        
-        switch (route.name) {
-            case "home":
-                iconName = "home"
-                break;
-            case "categories":
-                iconName = "appstore-o"
-                break;
-            case "cart":
-                iconName = "shoppingcart"
-                break;
-            case "account":
-                iconName = "user"
-                break;
-        }
-
-        return <AntDesign name={iconName} color={color} size={26} />
-    }
-
     return (
-        <Tab.Navigator 
-            screenOptions={{ headerShown: false }} 
-            tabBar={props => <BottomTabBar {...props} /> 
-        }>
+        <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={props => <BottomTabBar {...props} />}>
             <Tab.Screen options={{ title: "Inicio" }} name="home" component={Home} />
             <Tab.Screen options={{ title: "Categorias" }} name="categories" component={Categories} />
             <Tab.Screen options={{ title: "MiCarrito", tabBarBadge: 0 }} name="cart" component={Cart} />
@@ -47,18 +20,5 @@ const Tabs = () => {
         </Tab.Navigator>
     )
 }
-
-const styles = StyleSheet.create({
-    shadowBottomBar: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.36,
-        shadowRadius: 6.68,
-        elevation: 11,
-    }
-})
 
 export default Tabs;
